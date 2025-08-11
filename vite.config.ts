70be3df83629cb17b7b0b0b06d891608e6d423cd
git1gt <github1gt@oneco.ru>
2025-08-11 16:27:00 +0700

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: 'https://support360.1gt.ru',
   build: {
     rollupOptions: {
       output: {
